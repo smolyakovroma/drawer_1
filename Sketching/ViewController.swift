7f0529b00e8e0a57961128c5830f75a53f5f1928
romansmolakov <smolyakovroma@gmail.com>
2018-11-02 23:50:00 +0700

    Initial Commit

---
+++ Sketching/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sketching
+//
+//  Created by Роман Смоляков on 02/11/2018.
+//  Copyright © 2018 Роман Смоляков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
